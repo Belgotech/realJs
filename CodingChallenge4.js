df8f1a6907fb2e5280d0fd319cff2edf20d348d2
Belgotech <Belovedobiora2002@gmail.com>
2022-10-11 09:10:40 -0700

    progress

--- CodingChallenge4.js
+++ CodingChallenge4.js
@@ -1,43 +1,55 @@
-var markMass = 500; //kg
-var markHeight = 10; // meter
+// var markMass = 500; //kg
+// var markHeight = 10; // meter
 
-var johnMass = 1000; //kg
-var johnHeight = 10; //meter
+// var johnMass = 1000; //kg
+// var johnHeight = 10; //meter
 
-var markFinalHeight = markHeight * markHeight;
-console.log(markFinalHeight);
+// var markFinalHeight = markHeight * markHeight;
+// console.log(markFinalHeight);
 
-var johnFinalHeight = johnHeight * johnHeight;
-console.log(johnFinalHeight);
+// var johnFinalHeight = johnHeight * johnHeight;
+// console.log(johnFinalHeight);
 
-var markBMI = markMass / (markHeight * markHeight);
-var johnBMI = johnMass / (johnHeight * johnHeight);
-console.log(markBMI, johnBMI);
+// var markBMI = markMass / (markHeight * markHeight);
+// var johnBMI = johnMass / (johnHeight * johnHeight);
+// console.log(markBMI, johnBMI);
 
-if (markBMI > johnBMI) {
-  console.log("mark's BMI is greater than john's BMI");
-} else {
-  console.log("john's BMI is greater than mark's BMI");
-}
+// if (markBMI > johnBMI) {
+//   console.log("mark's BMI is greater than john's BMI");
+// } else {
+//   console.log("john's BMI is greater than mark's BMI");
+// }
 
 var mark = {
   firstName: "mark",
   lastName: "smith",
+  mass: 1000,
+  height: 10,
   calculateBMI: function () {
-    var markMass = 500; //kg
-    var markHeight = 10; // meter
-
-    var johnMass = 1000; //kg
-    var johnHeight = 10; //meter
-
-    var markFinalHeight = markHeight * markHeight;
-    console.log(markFinalHeight);
-
-    var johnFinalHeight = johnHeight * johnHeight;
-    console.log(johnFinalHeight);
-
-    var markBMI = markMass / (markHeight * markHeight);
-    var johnBMI = johnMass / (johnHeight * johnHeight);
-    console.log(markBMI, johnBMI);
+    this.BMI = this.mass / (this.height * this.height);
   },
 };
+
+mark.calculateBMI()
+// console.log(mark);
+
+var john = {
+    firstName: 'john',
+    lastName: 'smith',
+    mass: 1000,
+    height: 10,
+    calculateBMI: function(){
+    this.BMI = this.mass / (this.height * this.height);
+    }
+}
+
+john.calculateBMI()
+// console.log(john);
+
+if(john.BMI > mark.BMI){
+    console.log(john.firstName + ' has the higher BMI of ' + john.BMI);
+}else if(mark.BMI > john.BMI){
+    console.log(mark.firstName + ' has the higher BMI of ' + mark.BMI);
+}else{
+    console.log(mark.firstName + ' and ' + john.firstName + ' have the same BMI ');
+}
